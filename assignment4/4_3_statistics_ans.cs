d41c7eb7def5c3bfc0b154ecc7a86e64bd54c7fa
lemon-2727 <165107666+lemon-2727@users.noreply.github.com>
2024-11-04 16:09:31 +0900

    Update 4_3_statistics_ans.cs

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -20,6 +20,54 @@ namespace statistics
 
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
+            int stdCount = data.GetLength(0) - 1;
+            
+            double[] science = new double[stdCount];
+            double[] math = new double[stdCount];
+            double[] english = new double[stdCount];
+            double totalScore = 0;
+
+            for (int i = 1; i <= stdCount; i++){
+                math[i - 1] = double.Parse(data[i, 2]);
+                science[i - 1] = double.Parse(data[i, 3]);
+                english[i - 1] = double.Parse(data[i, 4]);
+            }
+
+            var studentScores = new (string Name, double TotalScore)[stdCount];
+          
+            for (int i = 1; i <= stdCount; i++){
+                totalScore = math[i - 1] + science[i - 1] + english[i - 1];
+                studentScores[i - 1] = (data[i, 1], totalScore);
+            }
+            
+            
+            int[] ranks = new int[stdCount];
+
+            for (int i = 0; i < stdCount; i++){
+                int rank = 1;
+                for (int j = 0; j < stdCount; j++){
+                    if (studentScores[i].TotalScore < studentScores[j].TotalScore) {
+                        rank++;
+                    }
+                }
+                ranks[i] = rank;
+            }
+
+            Console.WriteLine("Average Scores: ");
+            Console.WriteLine($"Math: {math.Average():F2}");
+            Console.WriteLine($"Science: {science.Average():F2}");
+            Console.WriteLine($"English: {english.Average():F2}");
+            Console.WriteLine();
+            Console.WriteLine("Max and min Scores: ");
+            Console.WriteLine($"Math: ({math.Max()}, { math.Min()})");
+            Console.WriteLine($"Science: ({science.Max()}, {science.Min()})");
+            Console.WriteLine($"English: ({english.Max()}, {english.Min()})");
+            Console.WriteLine();
+            Console.WriteLine("Students rank by total scores:");
+            for (int i = 0; i < stdCount; i++)
+            {
+                Console.WriteLine($"{studentScores[i].Name}: {ranks[i]}th");
+            }
             
             // --------------------
         }
