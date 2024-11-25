3be4e49a582ff4dc8353941c10d7a009edf60430
lemon-2727 <165107666+lemon-2727@users.noreply.github.com>
2024-11-25 16:42:04 +0900

    Update UIManager.cs

--- assignment7/Poketball Sample/Assets/Scripts/UIManager.cs
+++ assignment7/Poketball Sample/Assets/Scripts/UIManager.cs
@@ -12,6 +12,8 @@ public class UIManager : MonoBehaviour
     void Awake() {
         // MyText를 얻어오고, 내용을 지운다.
         // ---------- TODO ---------- 
+        MyText = GetComponentInChildren<Text>();
+        MyText.text = "";
         
         // -------------------- 
     }
@@ -19,8 +21,12 @@ public class UIManager : MonoBehaviour
     public void DisplayText(string text, float duration)
     {
         // NowCoroutine이 있다면 멈추고 새로운 DisplayTextCoroutine을 시작한다.
-        // ---------- TODO ---------- 
         
+        // ---------- TODO ---------- 
+        if (NowCoroutine != null){
+        StopCoroutine(NowCoroutine);
+        }
+        NowCoroutine = StartCoroutine(DisplayTextCoroutine(text, duration));
         // -------------------- 
     }
 
@@ -28,7 +34,8 @@ public class UIManager : MonoBehaviour
     {
         // MyText에 text를 duration초 동안 띄운다.
         // ---------- TODO ---------- 
-        yield return null; // dummy, remove this
+        MyText.text = text;
+        yield return new WaitForSeconds(duration);
         // -------------------- 
     }
 }
