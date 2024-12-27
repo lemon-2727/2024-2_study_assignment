7d1eba6a812a882e07dade28b43f67d5ff747352
lemon-2727 <165107666+lemon-2727@users.noreply.github.com>
2024-12-27 22:11:09 +0900

    Update MovementManager.cs

--- assignment8/Chess Sample/Assets/Scripts/MovementManager.cs
+++ assignment8/Chess Sample/Assets/Scripts/MovementManager.cs
@@ -22,6 +22,30 @@ public class MovementManager : MonoBehaviour
         // 보드에 있는지, 다른 piece에 의해 막히는지 등을 체크
         // 폰에 대한 예외 처리를 적용
         // --- TODO ---
+
+        (int, int) currentPos = piece.MyPos;
+        for (int i = 0; i <  moveInfo.distance; i++){
+            (int, int) nextPos = (currentPos.Item1 + moveInfo.dirX * (i+1), currentPos.Item2 + moveInfo.dirY * (i+1));
+            //Debug.Log("checkzero");
+            //Debug.Log(nextPos);
+            if (!Utils.IsInBoard(nextPos)) return false;
+            //Debug.Log("checkone");
+            if (gameManager.Pieces[nextPos.Item1, nextPos.Item2] != null){
+                if (gameManager.Pieces[nextPos.Item1, nextPos.Item2].PlayerDirection == piece.PlayerDirection) return false;
+                //Debug.Log("checktwo");
+                if (piece is Pawn)
+                {
+                    if (moveInfo.dirX != 1 && moveInfo.dirY != -1) return false;
+                    //Debug.Log("checkthree");
+                }
+            }
+            else if (piece is Pawn && (moveInfo.dirX == 1 || moveInfo.dirY == -1)) return false;
+            //Debug.Log("checkfour");
+            if (nextPos == targetPos) return true;
+            //Debug.Log("checkfive");
+        }
+        return false;
+    
         
         // ------
     }
@@ -30,19 +54,19 @@ public class MovementManager : MonoBehaviour
     private bool IsValidMoveWithoutCheck(Piece piece, (int, int) targetPos)
     {
         if (!Utils.IsInBoard(targetPos) || targetPos == piece.MyPos) return false;
-
+        
         foreach (var moveInfo in piece.GetMoves())
         {
             if (TryMove(piece, targetPos, moveInfo))
                 return true;
         }
-        
         return false;
     }
 
     // 체크를 포함한 상황에서 가능한 움직임인지를 검증
     public bool IsValidMove(Piece piece, (int, int) targetPos)
     {
+        
         if (!IsValidMoveWithoutCheck(piece, targetPos)) return false;
 
         // 체크 상태 검증을 위한 임시 이동
@@ -84,6 +108,18 @@ public class MovementManager : MonoBehaviour
         // 왕이 지금 체크 상태인지를 리턴
         // gameManager.Pieces에서 Piece들을 참조하여 움직임을 확인
         // --- TODO ---
+        for (int x = 0; x < Utils.FieldWidth; x++)
+        {
+            for (int y = 0; y < Utils.FieldHeight; y++)
+            {
+                var piece = gameManager.Pieces[x, y];
+                if (piece != null && piece.PlayerDirection != playerDirection)
+                {
+                    if (IsValidMoveWithoutCheck(piece, kingPos)) return true;
+                }
+            }
+        }
+        return false;
         
         // ------
     }
@@ -97,6 +133,18 @@ public class MovementManager : MonoBehaviour
         // effectPrefab을 effectParent의 자식으로 생성하고 위치를 적절히 설정
         // currentEffects에 effectPrefab을 추가
         // --- TODO ---
+        for (int x = 0; x < Utils.FieldWidth; x++)
+        {
+            for (int y = 0; y < Utils.FieldHeight; y++)
+            {
+                if (IsValidMove(piece, (x, y)))
+                {
+                    var effect = Instantiate(effectPrefab, effectParent);
+                    currentEffects.Add(effectPrefab);
+                }
+            }
+        }
+
         
         // ------
     }
